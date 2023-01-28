ee30a14331ce5be0677393923ca730af3c5f8788
LiveIsBigStange <98121339+LiveIsBigStange@users.noreply.github.com>
2023-01-28 10:12:53 +0300

    Addnew

--- src/main/java/solutions/javasoft/dao/model/Product.java
+++ src/main/java/solutions/javasoft/dao/model/Product.java
@@ -8,7 +8,6 @@ import java.util.List;
 @Entity
 public class Product {
     @Id
-
     private String code;
     private String name;
     private String description;
--- src/main/java/solutions/javasoft/dao/repository/ProductRepository.java
+++ src/main/java/solutions/javasoft/dao/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package solutions.javasoft.dao.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import solutions.javasoft.dao.model.Product;
+import java.util.List;
+
 
 public interface ProductRepository extends CrudRepository<Product, String>{
+    List<Product> findByCodeLike(String code);
 }
